0c5e735a11795dd1ce63a6cafc0f97abc0f4b452
Nikolay Brovkin <bronik004@yandex.ru>
2022-05-29 23:41:12 +0300

    fix: исправить затемнение попапа, добавить функцию рендера карточек

--- script/index.js
+++ script/index.js
@@ -1,13 +1,13 @@
-const popup = document.querySelectorAll(".popup");
+const popupList = document.querySelectorAll(".popup");
 const popupEdit = document.querySelector(".popup_type_edit");
 const popupAdd = document.querySelector(".popup_type_add");
-const editButton = document.querySelector(".profile__edit-button");
+const profileButton = document.querySelector(".profile__edit-button");
 const addButton = document.querySelector(".profile__add-button");
 const closeButton = document.querySelectorAll(".popup__close-button");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
-const popupName = document.querySelector(".popup__text-field_type_name");
-const popupDescription = document.querySelector(
+const popupName = popupEdit.querySelector(".popup__text-field_type_name");
+const popupDescription = popupEdit.querySelector(
   ".popup__text-field_type_description"
 );
 const imageButton = document.querySelector(".element__img");
@@ -16,8 +16,17 @@ const popupCaption = document.querySelector(".popup__caption");
 const popupTypeZoom = document.querySelector(".popup_type_zoom");
 const elementText = document.querySelector(".element__text");
 
-const formElement = document.querySelector(".popup__form");
+const formElement = document.querySelector(".popup__form_type_edit");
 const popupTypeEdit = document.querySelector(".popup_type_edit");
+// Переиминовал переменную
+const cardsContainer = document.querySelector(".elements");
+// Вынес переменную из функции createCards
+const cardTemplate = document.querySelector("#card-template").content;
+// переменные для добавления
+const popupTypeAdd = document.querySelector(".popup_type_add");
+const addNewPlace = document.querySelector(".popup__form_type_add-img");
+const placeName = document.querySelector(".popup__text-field_type_place-name");
+const placeLink = document.querySelector(".popup__text-field_type_place-link");
 
 // 1. Работа модальных окон. Открытие и закрытие модального окна
 
@@ -29,6 +38,17 @@ function openPopup(popup) {
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
+
+// Функция добавления лайка
+function addHeart(evt) {
+  evt.target.classList.toggle("element__heart_active");
+}
+
+// Функция удаления карточки
+function removeCard(evt) {
+  evt.target.closest(".element").remove();
+}
+
 // Функция открытия Popup профиль
 function openPropfilePopup() {
   popupName.value = profileName.textContent;
@@ -36,7 +56,7 @@ function openPropfilePopup() {
   openPopup(popupEdit);
 }
 
-editButton.addEventListener("click", openPropfilePopup);
+profileButton.addEventListener("click", openPropfilePopup);
 
 closeButton.forEach((item) => {
   item.addEventListener("click", () => closePopup(item.closest(".popup")));
@@ -55,57 +75,59 @@ formElement.addEventListener("submit", formSubmitHandler);
 // 2. Шесть карточек «из коробки»
 // https://cuva.ru/blog/20-udivitelnyh-mest-rossii
 
-let elements = document.querySelector(".elements");
 // Создаем новую карточку
 function createCards(cardName, cardImgLink) {
-  const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.querySelector(".element").cloneNode(true);
+  const elementImage = cardElement.querySelector(".element__img");
 
   cardElement.querySelector(".element__text").textContent = cardName;
-  cardElement.querySelector(".element__img").src = cardImgLink;
+  elementImage.src = cardImgLink;
+  elementImage.alt = cardName;
+
   // Добавление лайков
-  // ?Почему такой странный перенос строк? Как исправить?
   cardElement
     .querySelector(".element__heart")
-    .addEventListener("click", function (evt) {
-      evt.target.classList.toggle("element__heart_active");
-    });
+    .addEventListener("click", addHeart);
+
   // Удаление карточки
   cardElement
     .querySelector(".element__trash-button")
-    .addEventListener("click", function (evt) {
-      evt.target.closest(".element").remove();
-    });
-  // Зум карточки
-  cardElement
-    .querySelector(".element__img")
-    .addEventListener("click", function (evt) {
-      popupTypeZoom.classList.add("popup_opened");
-      popupImage.src = cardImgLink;
-      popupCaption.textContent = cardName;
-    });
+    .addEventListener("click", removeCard);
 
-  elements.prepend(cardElement);
+  // Зум карточки
+  elementImage.addEventListener("click", function () {
+    openPopup(popupTypeZoom);
+    popupImage.src = cardImgLink;
+    popupCaption.textContent = cardName;
+    popupImage.alt = cardName;
+  });
+
+  return cardElement;
+}
+
+function renderCard(name, url, contanier) {
+  const card = createCards(name, url);
+  contanier.prepend(card);
 }
 
 initialCards.forEach((item) => {
-  createCards(item.name, item.link);
+  renderCard(item.name, item.link, cardsContainer);
 });
 
 // 3. Форма добавления карточки
 
 addButton.addEventListener("click", function () {
-  popupAdd.classList.add("popup_opened");
+  openPopup(popupAdd);
 });
 
 // 4. Добавление карточки
-const popupTypeAdd = document.querySelector(".popup_type_add");
-const addNewPlace = document.querySelector(".popup__save-button_type_img");
-const placeName = document.querySelector(".popup__text-field_type_place-name");
-const placeLink = document.querySelector(".popup__text-field_type_place-link");
 
-addNewPlace.addEventListener("click", function (evt) {
+function addSubmitHandler(evt) {
   evt.preventDefault();
-  createCards(placeName.value, placeLink.value);
-  popupTypeAdd.classList.remove("popup_opened");
-});
+  renderCard(placeName.value, placeLink.value, cardsContainer);
+  placeName.value = "";
+  placeLink.value = "";
+  closePopup(popupTypeAdd);
+}
+
+addNewPlace.addEventListener("submit", addSubmitHandler);

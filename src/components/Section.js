742e52d5f5c5e4e9c45934fd2242cc74e4ee6135
Nikolay Brovkin <bronik004@gmail.com>
2022-07-22 21:52:41 +0700

    feat: Добавить класс Section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,20 @@
 export default class Section {
-  constructor() {
-
+  constructor({ items, renderer }, contanierSelector) {
+    this._renderedItems = items;
+    this._renderer = renderer;
+    this._contanier = document.querySelector(contanierSelector);
   }
+
+  renderItems() {
+    this._renderedItems.reverse().forEach((item) => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(card) {
+    this._container.prepend(card);
+  }
+
+
 };
 
